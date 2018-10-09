7850740a63ef07504c8dfe412796a5869d491b6d
Jackson Turner <jjtparadox@gmail.com>
2018-10-09 18:00:45 -0500

    Add api endpoint

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ def initialize():
 
 @app.route('/')
 def home():
-    # link = url_for("people")
-    # return f'Hello World! <a href="{link}">people</a>'
     return render_template("home.html")
 
 
@@ -42,33 +40,18 @@ def thanks():
                         (name, participation, last_participated, participation_info, learn_more,))
     return render_template("thanks.html")
 
-# @app.route('/people', methods=['GET', 'POST'])
-# def people():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         app.logger.info(f"got a name: {name}")
-#         with db.get_db_cursor(commit=True) as cur:
-#             cur.execute("insert into person (name) values (%s)", (name,))
-#         return redirect(url_for("people"))
-#     else:
-#         with db.get_db_cursor() as cur:
-#             cur.execute("SELECT * FROM person;")
-#             names = [record["name"] for record in cur]
-#
-#         return render_template("people.html", names=names)
 
-
-@app.route('/api/foo')
-def api_foo():
-    data = {
-        "message": "hello, world",
-        "isAGoodExample": False,
-        "aList": [1, 2, 3],
-        "nested": {
-            "key": "value"
-        }
-    }
-    return jsonify(data)
+@app.route('/api/results')
+def api_results():
+    with db.get_db_cursor() as cur:
+        cur.execute("select * from results")
+        rows = cur.fetchall()
+    if 'reverse' in request.args and request.args['reverse'] == 'true':
+        rows = list(reversed(rows))
+    results = []
+    for item in rows:
+        results.append(dict(item))
+    return jsonify(results)
 
 
 if __name__ == '__main__':
