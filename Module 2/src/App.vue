bc62eca3385b094a5d6c06b9c20338c8cc60ff14
Jackson Turner <jjtparadox@gmail.com>
2019-02-06 10:54:22 -0600

    Merge project from module 2

---
+++ Module 2/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <nav>
+            <Auth/>
+        </nav>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    import Auth from "@/components/Auth.vue"
+
+    export default {
+        name: 'app',
+        components: {
+            Auth
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+    #app nav {
+        text-align: right;
+        margin-bottom: 20px;
+    }
+</style>
---
+++ Module 2/src/firebaseConfig.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// https://cli.vuejs.org/guide/mode-and-env.html
+const config = {
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DB_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MSG_SENDER_ID
+};
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+const settings = { timestampsInSnapshots: true };
+db.settings(settings);
+
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
